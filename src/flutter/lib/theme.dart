0d0d4cea42bf61463d8c8a3c60140f0bb6b8e148
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2025-07-07 22:38:48 +0100

    Organize Flutter project structure and add theme

---
+++ src/flutter/lib/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final ColorScheme lightColorScheme = ColorScheme.fromSeed(seedColor: Colors.teal);
+final ColorScheme darkColorScheme =
+    ColorScheme.fromSeed(seedColor: Colors.teal, brightness: Brightness.dark);
+
+final ThemeData lightTheme = ThemeData(
+  colorScheme: lightColorScheme,
+  useMaterial3: true,
+);
+
+final ThemeData darkTheme = ThemeData(
+  colorScheme: darkColorScheme,
+  useMaterial3: true,
+);
