701bfbe109f5391bf98680d8dc1fc2a4e4c0516e
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2025-07-11 12:18:29 +0200

    Add 404 page and tests

---
+++ apps/barnostri_app/lib/src/features/home/presentation/pages/not_found_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: const Center(
+        child: Text('Page not found'),
+      ),
+    );
+  }
+}
