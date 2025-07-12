65db1019f438481d352493dee8d7e6712c0b1d12
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2025-07-12 20:41:17 +0200

    chore: setup golden tests

---
+++ apps/barnostri_app/test/golden/simple_golden_test.dart
@@ -0,0 +1,18 @@
+@Tags(['golden'])
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('simple golden', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(body: Text('Hello')),
+      ),
+    );
+
+    await expectLater(
+      find.byType(MaterialApp),
+      matchesGoldenFile('goldens/simple.png'),
+    );
+  });
+}
